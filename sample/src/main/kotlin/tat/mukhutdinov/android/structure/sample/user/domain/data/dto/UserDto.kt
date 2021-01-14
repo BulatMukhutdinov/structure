ebb2e64df8d4c03761bf551279c244b89251320b
Bulat Mukhutdinov <bulatmm@yandex.ru>
2021-01-14 14:10:18 +0300

    keep data binding classes

--- sample/src/main/kotlin/tat/mukhutdinov/android/structure/sample/user/domain/data/dto/UserDto.kt
+++ sample/src/main/kotlin/tat/mukhutdinov/android/structure/sample/user/domain/data/dto/UserDto.kt
@@ -1,7 +1,9 @@
 package tat.mukhutdinov.android.structure.sample.user.domain.data.dto
 
+import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
 
+@Keep
 class UserDto(
     @SerializedName("id")
     val id: Int?,
