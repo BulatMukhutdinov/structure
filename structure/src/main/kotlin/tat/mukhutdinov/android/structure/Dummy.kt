03f29a7a0f218676f20a7d297fd5a4c4570a1dea
BulatMukhutdinov <Bulatmm@yandex.ru>
2021-02-12 10:25:47 +0300

    rename Dummy to DummyBindings

--- structure/src/main/kotlin/tat/mukhutdinov/android/structure/Dummy.kt
+++
@@ -1,8 +0,0 @@
-package tat.mukhutdinov.android.structure
-
-/**
- *  Some classes like [StructureFragment] require generated [BR] class
- *  and its variables to set these variables in the layout for the data binding.
- *  [Dummy] is used as a stub to generate the required variables
- */
-interface Dummy
---
+++ structure/src/main/kotlin/tat/mukhutdinov/android/structure/DummyBindings.kt
@@ -0,0 +1,8 @@
+package tat.mukhutdinov.android.structure
+
+/**
+ *  Some classes like [StructureFragment] require generated [BR] class
+ *  and its variables to set these variables in a layout for the data binding.
+ *  [DummyBindings] is used as a stub to generate required variables.
+ */
+interface DummyBindings
